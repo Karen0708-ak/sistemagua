f523f2569508b5fef70b5ebeb955584c881be00a
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-05-22 09:45:28 -0500

    Se realizo las migraciones del modelo InstalacionCaptacion

--- Aplicaciones/Instalacion/models.py
+++ Aplicaciones/Instalacion/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from Aplicaciones.Empleado.models import Empleado
+from Aplicaciones.Casa.models import Casa
 
 # Create your models here.
+class InstalacionCaptacion(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    
+    # Relaciones
+    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    casa = models.ForeignKey(Casa, on_delete=models.CASCADE)
