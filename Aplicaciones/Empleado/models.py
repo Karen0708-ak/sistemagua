e77d237ac0ea2f9a553723b12cf90577eb6fd030
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-05-22 08:59:05 -0500

    Se realizaron las migraciones de la aplicacion Empleado

--- Aplicaciones/Empleado/models.py
+++ Aplicaciones/Empleado/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Empleados(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    cedula = models.CharField(max_length=20, unique=True)
+    especialidad = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20)
