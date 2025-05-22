25cdaa48c26741e9d2f665f54d8ccd99ad0bebc3
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-05-22 07:47:38 -0500

    Se agrego la aplicacion al archivo app de Instalacion

--- Aplicaciones/Instalacion/apps.py
+++ Aplicaciones/Instalacion/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class InstalacionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Instalacion'
+    name = 'Aplicaciones.Instalacion'
